3a2bb9a5319848b70ec6806d4370597110f96bc9
kaushalraykar <kaushal.raykar7171@gmail.com>
2017-10-01 13:24:32 +0530

    firebase added

---
+++ app/src/main/java/com/example/kaushal/notifier/MyConstant.java
@@ -0,0 +1,12 @@
+package com.example.kaushal.notifier;
+
+/**
+ * Created by kaushal on 01/10/2017.
+ */
+
+public class MyConstant {
+                                           //  0
+    public static final String[] EDU_SEC= {"Select class","FY BCS", "SY BCS", "TY BCS","MCS 1","MCS 2"};
+
+
+}
--- app/src/main/java/com/example/kaushal/notifier/RegisterActivity.java
+++ app/src/main/java/com/example/kaushal/notifier/RegisterActivity.java
@@ -6,17 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
 
-
+    DatabaseReference STUDENT_REFERENCE;
+    DatabaseReference TEACHER_REFERENCE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        STUDENT_REFERENCE= FirebaseDatabase.getInstance().getReference("demo");
+
     }
 
-    public void registerdone(View view) {
+    public void registerOnClick(View view) {
+
+        //validation - not empty
+
+
+        String ID=STUDENT_REFERENCE.push().getKey();
+        STUDENT_REFERENCE.child(ID).setValue("kunal");
+        //
+
         Intent intent=new Intent(this,LoginActivity.class);
         startActivity(intent);
         Toast.makeText(this, "Register Sucessfully", Toast.LENGTH_SHORT).show();
@@ -25,4 +40,6 @@ public class RegisterActivity extends AppCompatActivity {
 
 // spinner listener -----  https://stackoverflow.com/questions/1337424/android-spinner-get-the-selected-item-change-event
     // how visible roll number.... RrollNumber.setVisbility(View.VISIBLE)
+
+    // add another spinner for edu_type- bcs msc
 }
